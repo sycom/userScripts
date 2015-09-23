f37c4223fe7167fdab94520ebc859b8fe4c7bc9f
Sylvain Comte <sylvain.comte@yahoo.fr>
2015-09-23 19:54:26 +0200

    modified @match because multiple stars seems to broke it

--- OutOfAmazon.user.js
+++ OutOfAmazon.user.js
@@ -5,7 +5,7 @@
 // @downloadURL https://git.framasoft.org/sycom/userScripts/raw/master/OutOfAmazon.user.js
 // @updateURL	https://git.framasoft.org/sycom/userScripts/raw/master/OutOfAmazon.meta.js
 // @include     *://*.amazon.fr*
-// @match     	*://*.amazon.fr*
+// @match     	*
 // @author	    Sylvain Comte
 // @version     0.2
 // @require     https://cdn.jsdelivr.net/jquery/2.1.4/jquery.min.js
