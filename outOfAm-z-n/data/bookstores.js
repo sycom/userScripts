189d360e4167293ffd7f7761c90d548daa08c849
Sylvain Comte <sylvain.comte@gmail.com>
2016-04-20 20:56:04 +0200

    mise à jour des index (liens de traductions dans le head) début de la construction de la carte des boutiques (rapatrié de know-rmandie/ville-durable). Ne marche pas pour l'instant

---
+++ outOfAm-z-n/data/bookstores.js
@@ -0,0 +1,16 @@
+var data={
+	"date":"20 avril 2016",
+	"source":"<a href='https://sycom.github.io/userScripts/outOfAm-z-n'>Out of Am*z*n userscript</a> community",
+	"liste":[
+        {
+        "name": "Quai des mômes",
+        "url": "http://www.librairie-quaidesmomes.com",
+        "search": "http://www.librairie-quaidesmomes.com/vel/recherche/resultats-recherche-rapide.html?search_keys=",
+        "lat": "49.2132699",
+        "long": "1.1703296",
+        "city": "Louviers",
+        "country": "France",
+        "phone": ""
+        }
+    ]
+}
